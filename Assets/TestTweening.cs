dcc608b3462153156562e9a70af827968e4386e2
douduck08 <lu10082000@gmail.com>
2018-04-21 19:20:53 +0800

    use AnimationCurve as tweening path

---
+++ Assets/TestTweening.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTweening : MonoBehaviour {
+
+	public TweenPath path;
+	public float duration = 1f;
+
+	float time;
+	void Update () {
+		this.transform.position = path.Evaluate (time / duration);
+		time += Time.deltaTime;
+		time %= duration;
+	}
+}
---
+++ Assets/TweenPath.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenPath : MonoBehaviour {
+
+	public AnimationCurve x;
+	public AnimationCurve y;
+	public AnimationCurve z;
+	public Bounds bounds;
+
+	Matrix4x4 m_matrix;
+
+	private void Awake() {
+		 UpdateMatrix ();
+	}
+
+	public void UpdateMatrix () {
+		m_matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+	}
+
+	public Vector3 Evaluate (float t) {
+		Vector3 pos = bounds.min;
+		pos.x += x.Evaluate (t) * bounds.size.x;
+		pos.y += y.Evaluate (t) * bounds.size.y;
+		pos.z += z.Evaluate (t) * bounds.size.z;
+		pos = m_matrix.MultiplyPoint (pos);
+		return pos;
+	}
+
+	public Vector3 LocalEvaluate (float t) {
+		Vector3 pos = bounds.min;
+		pos.x += x.Evaluate (t) * bounds.size.x;
+		pos.y += y.Evaluate (t) * bounds.size.y;
+		pos.z += z.Evaluate (t) * bounds.size.z;
+		return pos;
+	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmos () {
+		UpdateMatrix ();
+		Gizmos.matrix = m_matrix;
+		Gizmos.color = Color.red;
+		for (int i = 0; i < 100; i++) {
+			Gizmos.DrawLine (LocalEvaluate(0.01f * i), LocalEvaluate(0.01f * i + 0.01f));
+		}
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube (bounds.center, bounds.size);
+	}
+#endif
+}
